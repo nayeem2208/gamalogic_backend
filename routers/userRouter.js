13b505d7370ff19a5c0e87a43cbc0ec185f72ff6
Nayeem <nayeem2281998@gmail.com>
2024-04-16 17:22:07 +0530

    batch email validation setup

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -2,7 +2,6 @@ import express from 'express'
 import Authentication from '../controllers/userAuthController.js';
 import APIControllers from '../controllers/APIController.js';
 import authcheck from '../middlewares/auth.js';
-import upload from '../utils/Multer.js';
 
 const router=express.Router()
 
@@ -17,8 +16,10 @@ router.post('/resetPassword',Authentication.resetPassword)
 
 router.post('/singleEmailValidator',authcheck,APIControllers.emailValidation)
 router.post('/singleEmailFinder',authcheck,APIControllers.FindSingleEmail)
-router.post('/batchEmailVerification',authcheck,upload.single('file'),APIControllers.batchEmailValidation)
+router.get('/getAllUploadedEmailValidationFiles',authcheck,APIControllers.getAlreadyCheckedBatchEmailFiles)
+router.post('/batchEmailVerification',authcheck,APIControllers.batchEmailValidation)
 router.get('/getBatchStatus',authcheck,APIControllers.batchEmailStatus)
+router.get('/downloadEmailVerificationFile',authcheck,APIControllers.downloadEmailVerificationFile)
 router.get('/getApiKey',authcheck,APIControllers.getApi)
 router.get('/resetApiKey',authcheck,APIControllers.resetApiKey)
 router.post('/changePassword',authcheck,APIControllers.changePassword)
