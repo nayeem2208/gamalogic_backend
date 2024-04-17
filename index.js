08746eb296d00aca7735dad4bcbcacc19a12f1f5
Nayeem <nayeem2281998@gmail.com>
2024-04-17 14:48:56 +0530

    batch email validation completed

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const port = process.env.PORT || 3000;
 
 const app = express();
 
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan("dev"));
 app.use(cookieParser());
